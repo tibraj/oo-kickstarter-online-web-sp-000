cf63365c2b70f3b077db449440ef21057e9c2cb2
Taulant Ibraj <tibraj94@gmail.com>
2020-04-20 15:07:09 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,7 @@ class Project
   end
 
   def add_backer(backer)
-    @backers << backer 
+    @backers << backer
+    backer.add_project(self) unless backer.backed_projects.include?(self) 
   end 
 end
