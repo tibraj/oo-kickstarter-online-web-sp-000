c4b0d283a3a9c8dc8abe838b9eab3062f4cd9d26
Taulant Ibraj <tibraj94@gmail.com>
2020-04-20 14:50:43 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,9 @@
-class Backer 
-  attr_accessor :name 
+class Backer
+  attr_accessor :name
   attr_reader :  :backed_projects
 
   def initialize(name)
-    @name = name 
+    @name = name
     @backed_projects = []
-  end 
+  end
 end
---
+++ lib/project.rb
@@ -0,0 +1,9 @@
+class Project
+  attr_accessor :title
+  attr_reader :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+end
